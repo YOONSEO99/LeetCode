8b3770e6317eabc710cf7eb02b5369b11e0fea69
Angella <70619186+YOONSEO99@users.noreply.github.com>
2024-09-04 14:21:57 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (96.28%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // 0. indesOf 메소드를 이용하여 needle이 haystack 앞에서부터 처음 발견되는 인덱스 반환 
+        int result = haystack.indexOf(needle);
+        return result;
+    }
+}
