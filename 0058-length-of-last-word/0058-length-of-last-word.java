330f224e5099b652ba6dbe1799c1bf68a7ad9103
Angella <70619186+YOONSEO99@users.noreply.github.com>
2024-09-09 13:08:01 +0900

    Time: 1 ms (33.07%), Space: 41.2 MB (97.00%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // 0. 문장에서 가장 마지막 단어의 길이 반환
+        // 0. 문자열 s에서 마지막 단어의 길이 반환
         
+        // 1. 공백을 기준으로 문자열 분리
         String[] sChange = s.split(" ");
         int lo = sChange.length-1;
         String result = sChange[lo];        
